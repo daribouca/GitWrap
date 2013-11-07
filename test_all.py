6ded6163737e39e9a9ed36c8a4f55704fc74f392
unknown <owner@ZEBWSLEAS108569.idcn.mil.intra>
2013-11-07 22:30:59 +0100

    added test_all as stageholder

---
+++ test_all.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      owner
+#
+# Created:     07/11/2013
+# Copyright:   (c) owner 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+from exe_wrapper_test import *
